c5bdf8a5787a75f50871b67071fd2e445c1c2860
Jonathan Payne <jonathnp@bellsouth.net>
2026-01-23 00:12:15 -0500

    Flush out user db tests

--- tests/data/user.spec.ts
+++ tests/data/user.spec.ts
@@ -1,22 +1,39 @@
-import { expect, describe, beforeAll, test } from 'vitest'
+import { eq } from 'drizzle-orm'
+import { afterAll, expect, describe, beforeEach, test } from 'vitest'
 import { db, User, usersTable } from '../../src/data';
 
 describe('user db operations', async () => {
-  beforeAll(async () => {
+  const user: User = {
+    id: null,
+    firstName: "Jonathan",
+    lastName: "Payne",
+    email: "payne.jonathan777@gmail.com",
+    admin: false
+  };
+
+  beforeEach(async () => {
     db.delete(usersTable).execute();
   });
 
-  test('api can insert user', async () => {
-    const user: User = {
-      id: null,
-      firstName: "Jonathan",
-      lastName: "Payne",
-      email: "payne.jonathan777@gmail.com",
-      admin: false
-    };
+  test('db can insert and find user', async () => {
+    const insertResult = await db.insert(usersTable).values(user).returning({ insertedId: usersTable.id }).execute();
+    const userId = insertResult[0].insertedId;
 
-    expect(() => {
-      db.insert(usersTable).values(user).execute();
-    }).not.toThrow();
+    const selectResult = await db.select().from(usersTable).where(eq(usersTable.id, userId));
+    
+    expect(selectResult[0].id).toEqual(userId);
+  });
+
+  test('db can update user', async () => {
+    const insertResult = await db.insert(usersTable).values(user).returning({ insertedId: usersTable.id }).execute();
+    const userId = insertResult[0].insertedId;
+
+    const updateResult = await db.update(usersTable).set({ firstName: "Nathan" }).where(eq(usersTable.id, userId)).returning();
+    
+    expect(updateResult[0].firstName).toEqual("Nathan");
+  });
+
+  afterAll(async () => {
+    db.delete(usersTable).execute();
   });
 });
